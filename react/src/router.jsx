a7102964cd55a044691e4fe9a3701667c2e1d463
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-06-22 17:53:08 +0100

    layout login and registration

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -3,6 +3,7 @@ import Surveys from "./views/Surveys.jsx";
 import Dashboard from "./views/Dashboard.jsx";
 import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
+import GuestLayout from "./Components/GuestLayout.jsx";
 
 const router=createBrowserRouter(
   [
@@ -17,13 +18,20 @@ const router=createBrowserRouter(
     },
 
     {
-      path:"/login",
-      element:<Login/>
+      path:"/",
+      element:<GuestLayout/>,
+      children:[
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/signup",
+          element:<Signup/>
+        },
+      ]
     },
-    {
-      path:"/signup",
-      element:<Signup/>
-    },
+
     {
       path:"/dashboard",
       element:<Dashboard/>
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -9,18 +9,9 @@ export default function Signup() {
         <body class="h-full">
         ```
       */}
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Signup  For Free
           </h2>
-        </div>
-
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
@@ -79,7 +70,6 @@ export default function Signup() {
             </a>
           </p>
         </div>
-      </div>
     </>
   )
 }
