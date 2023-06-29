a285a5e41da922cdd6838688307b9c1529a37110
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-06-29 08:33:54 +0100

    env init

--- react/src/axios.js
+++ react/src/axios.js
@@ -1,5 +1,20 @@
 import axios from "axios";
+import router from "./router";
 
 const axiosClient=axios.create({
-  baseURL:'http://127.0.0.1:8080/api'
+  baseURL:`${import.meta.VITE_API_BASE_URL}/api`
 })
+
+axiosClient.interceptors.request.use((config)=>{
+  const token="123";
+  config.headers.Authorization =`Bearer ${token}`
+})
+axiosClient.interceptors.response.use(response=>{
+  return response;
+},error=>{
+  if(error.response && error.response.status=== 401){
+    router.navigate('/login')
+    return error;
+  }
+})
+export default axiosClient;
