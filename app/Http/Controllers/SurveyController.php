03afd1d9eab743cba523387c0ca4d08ad2387333
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-08-17 12:12:25 +0100

    resource init and valid image

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\SurveyStoreRequest;
-use App\Http\Requests\SurveyUpdateRequest;
 use App\Models\Survey;
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
+use App\Http\Resources\SurveyResource;
 use Illuminate\Http\Request;
 
 class SurveyController extends Controller
@@ -12,33 +13,52 @@ class SurveyController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user=$request->user();
+
+        return SurveyResource::collection(Survey::where('user_id',$user->id)->orderBy('created_at','desc')->paginate(10));
     }
+
+    /**
+     * Show the form for creating a new resource.
+     */
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(SurveyStoreRequest $request)
+    public function store(StoreSurveyRequest $request)
     {
-        $user=$request->user();
+        $data=$request->validated();
 
-        return Survey::where('user_id',$user->id)->orderBy('created_at','desc')->get();
+        if(isset($data['image'])){
+            $relativePath=$this->saveImage($data['image']);
+            $data['image']=$relativePath;
+        }
+
+        $survey=Survey::create($data);
+
+        foreach($data['questions'] as $question){
+            $question['survey_id'] = $survey->id;
+            $this->createQuestion($question);
+        }
+
+        return new SurveyResource($survey);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Survey $survey)
     {
         //
     }
 
+
     /**
      * Update the specified resource in storage.
      */
-    public function update(SurveyUpdateRequest $request, string $id)
+    public function update(UpdateSurveyRequest $request, Survey $survey)
     {
         //
     }
@@ -46,8 +66,31 @@ class SurveyController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Survey $survey)
     {
         //
+    }
+
+    private function saveImage($image){
+
+        // check if image is valid base64 string
+        if(preg_match('/^data:image\/(w+);base64,/',$image,$type)){
+            // take out the base64 encoded text without mime type
+            $image=substr($image,strpos($image,',') +1);
+            // Get File extension
+            $type=strtolower($type[1]); // jpg,png,gif
+
+            // Check if file is an image
+            if(!in_array($type,['jpg','jpeg','gif','png'])){
+                throw new \Exception('invalid image type');
+            }
+
+            $image=str_replace(' ','+',$image);
+            $image=base64_decode($image);
+
+            if($image === false){
+                throw new \Exception('base64_decode failed');
+            }
+        }
     }
 }
--- database/migrations/2023_08_17_075141_create_survey_questions_table.php
+++ database/migrations/2023_08_17_075141_create_survey_questions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Survey;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('surveys', function (Blueprint $table) {
+        Schema::create('survey_questions', function (Blueprint $table) {
             $table->id();
+            $table->string('type',45);
+            $table->string('question',2000);
+            $table->longText('description')->nullable();
+            $table->longText('data')->nullable();
+            $table->foreignId(Survey::class,'survey_id');
             $table->timestamps();
         });
     }
@@ -22,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('surveys');
+        Schema::dropIfExists('survey_questions');
     }
 };
